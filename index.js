241f0e3462ea31ee04cfb546d2605dcb2e9fb619
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-19 14:11:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber) {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    cart = [];
+    var cart = [];
   }
   else {
       return "Sorry, we don't have a credit card on file for you.";
