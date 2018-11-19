3b000a889b8453a58e39d1134bfe152f44ce490a
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-19 14:11:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber) {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     cart = [];
   }
   else {
