8f004305adca99c08baf2a482be18cde0d8021ac
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-19 14:13:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,26 +62,14 @@ function removeFromCart(item) {
   }
   return "That item is not in your cart."
 }
+
 function placeOrder(cardNumber) {
-  if(cardNumber) 
-  {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    for (var i = cart.length; i>= 0; i--)
-    { cart.pop();} 
+//   write your code here
+  if (cardNumber) {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
   }
-  else 
-  {
-    console.log('Sorry, we don\'t have a credit card on file for you.')
-    return cart;
+  else {
+      return "Sorry, we don't have a credit card on file for you.";
+    }
   }
-}
-//function placeOrder(cardNumber) {
-  // write your code here
-//  if (cardNumber) {
-  //  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-    //cart = [];
-  //}
-  //else {
-    //  return "Sorry, we don't have a credit card on file for you.";
-    //}
-  //}
